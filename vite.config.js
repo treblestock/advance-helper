37709b70f4b03401d7600844f5ca1a1fd56dea54
Fedor <verstalshitsya@gmail.com>
2022-12-20 15:30:37 +0300

    builded into docs

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,28 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
-    },
-  },
+export default defineConfig(({command}) => {
+  if (command === 'serve') {
+    return ({
+      plugins: [vue()],
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, 'src')
+        },
+      },
+    })
+  } else {
+    return ({
+      plugins: [vue()],
+      base: '/advance-helper/',
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, 'src')
+        },
+      },
+      // build: {
+      //   outDir: '../dist'
+      // },
+    })
+  }
 })
